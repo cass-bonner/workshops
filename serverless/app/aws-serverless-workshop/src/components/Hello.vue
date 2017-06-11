d2589d98fd120867368820dc834673045ef0c30c
Bonner, Cassandra <cass_bonner@yahoo.com.au>
2017-06-11 19:11:56 +1000

    First checkin for serverless workshop

---
+++ serverless/app/aws-serverless-workshop/src/components/Hello.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h4>Service & Framework Utilized</h4>
+    <table cellpadding="10px"  align="center">
+      <tr>
+        <td width="50%"><img src="../assets/s3.png"/><br/><a href="http://docs.aws.amazon.com/AmazonS3/latest/gsg/GetStartedWithS3.html">Amazon S3</a></td>
+<td width="50"><img src="../assets/logo.png"/><br/><a href="https://vuejs.org/">Vue.js</a></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to the AWS Serverless Workshop'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import '../assets/app.css';
+</style>
